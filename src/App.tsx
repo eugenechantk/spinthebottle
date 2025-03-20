05ccb6d7c834e4bc970106a302b98592b5fa30b7
eugenechantk <me@eugenechantk.me>
2025-03-20 14:43:23 +0800

    adding all the components together

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import { Cards } from "./component/Cards";
+import Spinner from "./component/Spinner";
 
 function App() {
   return (
     <>
+      <Spinner />
+      <Cards numCards={5} />
       <h1 className="text-3xl font-bold underline">Vite + React</h1>
     </>
   );
