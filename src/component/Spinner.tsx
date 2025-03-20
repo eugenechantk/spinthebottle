533b3caa8bd5e7441822df550a2c1118c5ee4841
eugenechantk <me@eugenechantk.me>
2025-03-20 15:18:23 +0800

    realistic beer bottle

--- src/component/Spinner.tsx
+++ src/component/Spinner.tsx
@@ -5,15 +5,15 @@ export default function Spinner() {
   const controls = useAnimationControls();
 
   const handleClick = async () => {
-    // Generate random number of spins between 4 and 6
-    const spins = 4 + Math.random() * 2;
-    const degrees = spins * 360;
+    // Generate random number of spins between 20 and 40
+    const spins = 20 + Math.random() * 20;
+    const degrees = spins * 360; // Remove the negative sign for clockwise rotation
 
     await controls.start({
       rotate: degrees,
       transition: {
-        duration: 3,
-        ease: [0.2, 0.8, 0.3, 1], // Similar to cubic-bezier
+        duration: 6,
+        ease: [0.2, 0.95, 0.1, 1],
         type: "tween",
       },
     });
@@ -21,15 +21,14 @@ export default function Spinner() {
 
   return (
     <div className="flex flex-1 min-h-0 justify-center items-center">
-      <motion.div
-        className="bottle"
+      <motion.img
+        src="/beer_bottle.png"
+        alt="Beer bottle"
+        className="beer-bottle"
         animate={controls}
         onClick={handleClick}
         initial={{ rotate: 0 }}
-      >
-        <div className="bottle-neck"></div>
-        <div className="bottle-body"></div>
-      </motion.div>
+      />
     </div>
   );
 }
